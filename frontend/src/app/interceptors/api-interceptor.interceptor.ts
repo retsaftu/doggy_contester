58dff0922f8e6e132335fd9f108f65a3c89f8550
Rafaello <baimurzinrafael02@gmail.com>
2022-03-26 19:28:05 +0600

    Added error and token interception

--- frontend/src/app/interceptors/api-interceptor.interceptor.ts
+++ frontend/src/app/interceptors/api-interceptor.interceptor.ts
@@ -6,13 +6,47 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../services/auth.service';
+import { tap, map } from 'rxjs';
+import { SnackBarService } from '../services/snack-bar.service';
 
 @Injectable()
 export class ApiInterceptorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService,
+    private snackBarService: SnackBarService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+  console.log(`[interceptor] Token: ${this.authService.getToken()}`)
+
+  const headersFroInterceptor: any = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET',
+  'Content-Type':  'application/json',
+  }
+
+  if(this.authService.isLoggedIn()) {
+    headersFroInterceptor[environment.tokenHeader] = this.authService.getToken();
+  }
+
+  const authReq = req.clone({
+    headers: new HttpHeaders(headersFroInterceptor)
+  });
+
+
+  return next.handle(authReq)
+        .pipe(
+          tap({
+            next: (event) => {},
+            error: (error) => {
+              console.log(`[interceptor] error ${error}`);
+              this.snackBarService.openErrorSnackBar(error.error.message);
+            }
+          })
+        );
   }
 }
