9a323bf66ff413c07a3857b500033a261f4c5a7e
Rafaello <baimurzinrafael02@gmail.com>
2022-04-03 23:38:51 +0600

    Added navigation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component'
+import { ContestComponent } from './components/contest/contest.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', component: AuthComponent },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'contest/:id',
+        component: ContestComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
