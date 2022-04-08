a8ca57c667d123860edcc892565f6233590f3df6
Rafaello <baimurzinrafael02@gmail.com>
2022-04-08 17:09:37 +0600

    Added avatar to the user page

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FileService } from 'src/app/services/file.service';
 import { UserService } from 'src/app/services/user.service';
+
+class FileSnippet {
+  pending: boolean = false;
+  status: string = 'init';
+
+  constructor(public src: string, public file: File) { }
+}
 
 @Component({
   selector: 'app-user',
@@ -12,10 +20,18 @@ export class UserComponent implements OnInit {
   public user: any
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private fileService: FileService
   ) { }
 
+  fileType = '';
+
+  selectedFile: FileSnippet | any;
+
+  filePath = '';
+
   ngOnInit(): void {
+    this.fileType = '.jpeg,.png,';
     let splittedUrl = this.router.url.split('/');
     console.log(`splittedUrl`, splittedUrl);
     const id = splittedUrl[splittedUrl.length - 1];
@@ -23,7 +39,51 @@ export class UserComponent implements OnInit {
     this.userService.getUserById(id).subscribe((res: any) => {
       console.log(`res`, res);
       this.user = res
+      this.filePath = this.user.avatar;
     });
   }
+
+  private onSuccess() {
+    this.selectedFile.pending = false;
+    this.selectedFile.status = 'ok';
+  }
+
+  private onError() {
+    this.selectedFile.pending = false;
+    this.selectedFile.status = 'fail';
+    this.selectedFile.src = '';
+  }
+
+  processFile(fileInput: any) {
+    const file: File = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+
+      this.selectedFile = new FileSnippet(event.target.result, file);
+
+      this.selectedFile.pending = true;
+      console.log(this.selectedFile);
+      this.fileService.uploadAvatar(this.selectedFile.file).subscribe(
+        (res: any) => {
+          this.onSuccess();
+          setTimeout(() => {
+            this.selectedFile = null;
+          }, 5000);
+          console.log(`res`, res);
+          this.filePath = res[0].avatar;
+          // res.data ? this.uploadResult.emit(res.data) : null;
+        },
+        (err: any) => {
+          this.onError();
+          setTimeout(() => {
+            this.selectedFile = null;
+          }, 5000);
+        })
+    });
+
+    reader.readAsDataURL(file);
+  }
+
 
 }
--- frontend/src/app/services/file.service.ts
+++ frontend/src/app/services/file.service.ts
@@ -15,12 +15,23 @@ export class FileService {
     console.log(`file`, file);
     console.log(`uId`, uId);
     let formData = new FormData();
-    formData.append('settings', JSON.stringify({ "userId": uId }));
     formData.append('file', file);
     formData.append('contestId', '60c09af61d2fc08732e62edd');
     formData.append('taskId', '60c09b891d2fc08732e62ede');
     formData.append('extension', '.js');
     console.log(`formData`, formData);
     return this.http.post('/api/file', formData);
+  }
+
+  uploadAvatar(file: File) {
+    console.log('uploadOne');
+    console.log(`file`, file);
+    let formData = new FormData();
+    formData.append('file', file);
+    // formData.append('contestId', '60c09af61d2fc08732e62edd');
+    // formData.append('taskId', '60c09b891d2fc08732e62ede');
+    // formData.append('extension', '.');
+    console.log(`formData`, formData);
+    return this.http.post('/api/file/image', formData);
   }
 }
