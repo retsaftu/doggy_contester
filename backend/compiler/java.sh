2df0655c68b157d020a7985629dc93d71aa11aaf
mendigali <mendigalitemir13@gmail.com>
2022-04-13 13:13:42 +0600

    Fixed cpp and python compiler errors, added broken java compiler

---
+++ backend/compiler/java.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+while getopts c:t:u: flag
+do
+    case "${flag}" in
+        c) contestId=${OPTARG};;
+        u) userId=${OPTARG};;
+        t) taskId=${OPTARG};;
+        *) echo "Usage: ./java.sh -c [contestId] -t [taskId] -u [userId]" && exit 1;;
+    esac
+done
+
+originalFile=$(find ./../uploads/${contestId}/${taskId}/${userId}/java/ -name '*.java')
+
+# Compile code, create executable and write compile errors to compile.log file
+# javac $originalFile \
+#     &> ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log
+
+# Check if compilation errors occurred and stop execution if they did
+if [ -s ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log ]; then
+    echo "Compilation failed" >> ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log
+    exit 1
+fi
+
+mkdir ./../uploads/${contestId}/${taskId}/${userId}/java/output
+mkdir ./../uploads/${contestId}/${taskId}/${userId}/java/result
+
+testsAmount=$(ls ./../uploads/${contestId}/${taskId}/${userId}/java/input | wc -l)
+
+# compiledFile=$(find ./../uploads/${contestId}/${taskId}/${userId}/java/ -name '*.class')
+
+# echo "Original file: ${originalFile}"
+# echo "Compiled file: ${compiledFile}"
+
+for i in $(seq 1 $testsAmount); do
+    START=$(date +%s.%3N)
+
+    # Run program
+    java $originalFile \
+        1> ./../uploads/${contestId}/${taskId}/${userId}/java/output/test${i}.out \
+        2> ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log
+
+    END=$(date +%s.%3N)
+
+    DIFF=$(echo "$END - $START" | bc)
+
+    # Check if test failed
+    if [ -s ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log ]; then
+        echo "Error while compiling test ${i}" >> ./../uploads/${contestId}/${taskId}/${userId}/java/compile.log
+    fi
+
+    j=1
+    isUserAnswerCorrect=false
+
+    while [[ -f ./../uploads/${contestId}/${taskId}/${userId}/java/correct/test${i}.answer${j}.out ]]
+    do
+        sed -i '$a\' ./../uploads/${contestId}/${taskId}/${userId}/java/correct/test${i}.answer${j}.out
+        sed -i '$a\' ./../uploads/${contestId}/${taskId}/${userId}/java/output/test${i}.out
+
+        # Compare user answer with correct answer
+        diff ./../uploads/${contestId}/${taskId}/${userId}/java/output/test${i}.out \
+            ./../uploads/${contestId}/${taskId}/${userId}/java/correct/test${i}.answer${j}.out \
+            &> ./../uploads/${contestId}/${taskId}/${userId}/java/result/test${i}.out
+
+        # Check if user answer is wrong or correct
+        if [ -s ./../uploads/${contestId}/${taskId}/${userId}/java/result/test${i}.out ]; then
+            isUserAnswerCorrect=false
+        else
+            isUserAnswerCorrect=true
+            echo "Correct answer" >> ./../uploads/${contestId}/${taskId}/${userId}/java/result/test${i}.out
+            break
+        fi
+
+        ((j++))
+    done
+
+    # Check if user answer is not correct
+    if [ "$isUserAnswerCorrect" = false ]; then
+        echo "Wrong answer" > ./../uploads/${contestId}/${taskId}/${userId}/java/result/test${i}.out
+    fi
+
+    echo ${DIFF} >> ./../uploads/${contestId}/${taskId}/${userId}/java/result/test${i}.out
+done
