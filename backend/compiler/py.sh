4ec1e41ef86d92a8178bb967e42ddeb288bcd11e
mendigali <mendigalitemir13@gmail.com>
2022-04-20 13:10:22 +0600

    py compiler infinite loop fixed

--- backend/compiler/py.sh
+++ backend/compiler/py.sh
@@ -10,9 +10,11 @@ do
     esac
 done
 
+touch ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log
+
 mkdir ./../uploads/${contestId}/${taskId}/${userId}/py/output
 mkdir ./../uploads/${contestId}/${taskId}/${userId}/py/result
-mkdir ./../uploads/${contestId}/${taskId}/${userId}/py/difference
+mkdir ./../uploads/${contestId}/${taskId}/${userId}/py/error
 
 testsAmount=$(ls ./../uploads/${contestId}/${taskId}/${userId}/py/input | wc -l)
 
@@ -20,19 +22,19 @@ for i in $(seq 1 $testsAmount); do
     START=$(date +%s.%3N)
 
     # Run program
-    python3 ./../uploads/${contestId}/${taskId}/${userId}/py/main.py \
+    ./timeout -t 1 -m 1024 python3 ./../uploads/${contestId}/${taskId}/${userId}/py/main.py \
         < ./../uploads/${contestId}/${taskId}/${userId}/py/input/test${i}.in \
         1> ./../uploads/${contestId}/${taskId}/${userId}/py/output/test${i}.out \
-        2> ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log
+        2> ./../uploads/${contestId}/${taskId}/${userId}/py/error/test${i}.err
 
     END=$(date +%s.%3N)
 
     DIFF=$(echo "$END - $START" | bc)
 
     # Check if test failed
-    if [ -s ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log ]; then
-        echo "Error while compiling test ${i}" >> ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log
-    fi
+    # if [ -s ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log ]; then
+    #     echo "Error while compiling test ${i}" >> ./../uploads/${contestId}/${taskId}/${userId}/py/compile.log
+    # fi
 
     j=1
     isUserAnswerCorrect=false
@@ -45,10 +47,10 @@ for i in $(seq 1 $testsAmount); do
         # Compare user answer with correct answer
         diff ./../uploads/${contestId}/${taskId}/${userId}/py/output/test${i}.out \
             ./../uploads/${contestId}/${taskId}/${userId}/py/correct/test${i}.answer${j}.out \
-            &> ./../uploads/${contestId}/${taskId}/${userId}/py/difference/test${i}.out -u
+            &> ./../uploads/${contestId}/${taskId}/${userId}/py/result/test${i}.out -u
 
         # Check if user answer is wrong or correct
-        if [ -s ./../uploads/${contestId}/${taskId}/${userId}/py/difference/test${i}.out ]; then
+        if [ -s ./../uploads/${contestId}/${taskId}/${userId}/py/result/test${i}.out ]; then
             isUserAnswerCorrect=false
         else
             isUserAnswerCorrect=true
